a6c9b59d42b8a0352109e1502707453b13219506
mgarciaaa2006-cyber <mgarciaaa2006-cyber>
2026-05-24 16:43:56 +0800

    Mechanism of 2D game

---
+++ src/GamePanel.java
@@ -0,0 +1,48 @@
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel implements Runnable{
+
+    //GAME SETTING
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    Thread  gameThread;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this); // "this" means the GamePanel
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while(gameThread != null) {
+
+            // 1 UPDATE: update information such as character position
+
+            // 2 DRAW: draw the screen with the updated information
+        }
+    }
+
+    public void update() {
+
+    }
+
+    public void paintComponent() {
+
+    }
+
+}
